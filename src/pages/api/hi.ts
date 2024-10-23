476f6017ee949f8f75a3df77c0f0d2710ea75328
thinh <thinhbuihong.ma@gmail.com>
2024-10-23 22:21:37 +0700

    feat: connect db wit prisma

--- src/pages/api/hi.ts
+++ src/pages/api/hi.ts
@@ -1,3 +1,6 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+import { prisma } from "../../lib/prisma";
+
+export default async function handler(req, res) {
+  const user = await prisma.user.findMany();
+  res.status(200).json({ name: "John Doe", user });
 }
